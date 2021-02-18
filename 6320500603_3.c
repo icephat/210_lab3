aeb69ae64e46daa17a5e8b2a27930cecbae01084
icephat <phattaraporn.sa@ku.th>
2021-02-18 17:13:19 +0700

    เพิ่มเงื่อนไข

--- 6320500603_3.c
+++ 6320500603_3.c
@@ -3,18 +3,27 @@ int main()
 {
     int a,n,i=2;
     scanf("%d",&n);
-    while(n/i>0)
+    if(n==1)
     {
-        if(n%i==0)
+        printf("1\n");
+    }
+
+    for(i=2;n!=1;i++)
+    {
+        while(n/i>0)
         {
-            printf("%d\n",i);
-            n=n/i;
-        }
-        else
-        {
-            i++;
+            if(n%i==0)
+            {
+                printf("%d\n",i);
+                n=n/i;
+            }
+            else
+            {
+                i++;
+            }
         }
     }
+
     printf("0");
     return 0;
 }
